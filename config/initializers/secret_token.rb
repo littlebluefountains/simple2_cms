3253bf02754a7b62289cb56f0ebe4949cf2d7fba
littlebluefountains <littlebluefountains@yahoo.com>
2011-07-13 22:21:56 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simple2Cms::Application.config.secret_token = '0e84105b16e7d83ef30cf4f87555e79a69e890cd5eec2a7d765d4bf9880a59c38d186ce17bd1d194f50d8c6f9af93b66053e27e87f10bb6f0082f0fc97a794f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Simple2Cms::Application.config.session_store :cookie_store, :key => '_simple2_cms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Simple2Cms::Application.config.session_store :active_record_store
